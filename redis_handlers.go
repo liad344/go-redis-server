77bdf895fe48718362795be368369d7f58238cb4
liad sagi <liad.sagi3@gmail.com>
2020-05-13 14:55:46 +0300

    .

--- redis_handlers.go
+++ redis_handlers.go
@@ -13,7 +13,7 @@ type Command struct {
 }
 
 type value struct {
-	data []byte
+	data      []byte
 	redisType string
 	//ttl time.Duration
 }
@@ -24,49 +24,51 @@ type RedisInstance struct {
 }
 
 func (i *RedisInstance) Ping(conn Conn, cmd Command) {
-	conn.Write(STRING , []byte("PONG"))
-	log.Info("Ponged " , conn.RemoteAddr() )
+	conn.Write(STRING, []byte("PONG"))
+	log.Info("Ponged ", conn.RemoteAddr())
 }
-
 
 func (i *RedisInstance) Del(conn Conn, cmd Command) {
 	if len(cmd.Args) < 2 {
-		conn.Write(ERROR , []byte("Wrong number of arguments"))
+		conn.Write(ERROR, []byte("Wrong number of arguments"))
 		return
 	}
 	i.Lock()
-	delete(i.data , key(cmd.Args[1]))
+	delete(i.data, key(cmd.Args[1]))
 	i.Unlock()
 	log.Info("Deleted")
 }
 
 func (i *RedisInstance) Get(conn Conn, cmd Command) {
 	if len(cmd.Args) < 2 {
-		conn.Write(ERROR , []byte("Not enough arguments"))
+		conn.Write(ERROR, []byte("Not enough arguments"))
 		return
 	}
 	key := key(cmd.Args[1])
 	i.Lock()
-	val , ok := i.data[key]
+	val, ok := i.data[key]
 	i.Unlock()
 	if ok {
-		conn.Write( val.redisType , val.data)
+		conn.Write(val.redisType, val.data)
+		conn.Close()
 		return
 	}
-	conn.Write(ERROR , []byte("Key not found"))
+	conn.Write(ERROR, []byte("Key not found"))
+	conn.Close()
 }
 
 func (i *RedisInstance) Set(conn Conn, cmd Command) {
 	if len(cmd.Args) < 3 {
-		conn.Write(ERROR , []byte("Not enough arguments"))
+		conn.Write(ERROR, []byte("Not enough arguments"))
 		return
 	}
-	val := value{data:cmd.Args[2] , redisType: STRING}
+	val := value{data: cmd.Args[2], redisType: STRING}
 	key := key(cmd.Args[1])
 
 	safeSet(i, key, val, cmd)
 
 	conn.Write(STRING, []byte("OK"))
+	conn.Close()
 	return
 }
 
@@ -74,7 +76,7 @@ func safeSet(i *RedisInstance, key key, val value, cmd Command) {
 	i.Lock()
 	i.data[key] = val
 	if len(cmd.Args) > 3 {
-		go i.deleteAfterTtl(key, cmd.Args[3] , cmd.Args[4])
+		go i.deleteAfterTtl(key, cmd.Args[3], cmd.Args[4])
 	}
 	i.Unlock()
 }
@@ -83,11 +85,10 @@ func (i *RedisInstance) deleteAfterTtl(k key, ttl []byte, timeFormat []byte) {
 	if string(timeFormat) != "px" || string(timeFormat) != "ex" {
 		return
 	}
-	tInt , err := strconv.ParseInt(string(ttl) , 10 , 8)
-	if err != nil{
+	tInt, err := strconv.ParseInt(string(ttl), 10, 8)
+	if err != nil {
 		log.Error("Could not parse ttl")
 	}
 	<-time.After(time.Second * time.Duration(tInt))
-	delete(i.data , k )
+	delete(i.data, k)
 }
-
