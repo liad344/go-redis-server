de50897a090e79b613b77472f173638939a2df56
liad sagi <liad.sagi3@gmail.com>
2020-05-13 09:07:53 +0300

    Added go.mod file for github.com/liad344/go-redis-server , added profiler

---
+++ go.mod
@@ -0,0 +1,8 @@
+module guthub.com/liad344/go-redis-server
+
+go 1.14
+
+require (
+	github.com/pkg/profile v1.4.0
+	github.com/sirupsen/logrus v1.6.0
+)
--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	_ "net/http/pprof"
+	"github.com/pkg/profile"
+)
+
 func main(){
+	defer profile.Start(profile.CPUProfile).Stop()
 	s := NewServer()
 	s.Init()
 
